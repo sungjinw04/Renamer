5b8c7b386a99750362b1e532b73eac8d0a1cd790
Aakash-prvt <akashpremiums03@gmail.com>
2024-07-02 22:24:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@ from config import Config
 from aiohttp import web
 from route import web_server
 import asyncio
+import pyrogram.utils
 
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 class Bot(Client):
 
     def __init__(self):
